d56cc67ff880be7789bc2f999d40034cd63a9a46
Kiat Lim <kiat@pixelonion.com>
2013-06-21 14:53:48 +0800

    test

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div id="page">
+		<div id="page">
+			<div class="logo"></div>
+			<ul id="navigation">
+				<li><a href=""><?php </a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
